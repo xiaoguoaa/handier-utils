eb22f82f6ec4d1de7d4be53183be1621ce5142c9
郭文豪 <973886860@qq.com>
2023-12-01 21:24:04 +0800

    feat: 添加 cloneDeep 函数

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import chunk from "./utils/chunk";
 import throttle from "./utils/throttle";
 import debounce from "./utils/debounce";
+import cloneDeep from "./utils/cloneDeep";
 
-export { chunk, throttle, debounce };
+export { chunk, throttle, debounce, cloneDeep };
---
+++ src/utils/cloneDeep.ts
@@ -0,0 +1,28 @@
+/**
+ * 深拷贝
+ * @param source 要拷贝的源对象
+ * @returns 返回拷贝后的目标对象
+ */
+function cloneDeep<T>(source: T): T {
+  if (source === null || typeof source !== "object") {
+    return source;
+  }
+
+  if (Array.isArray(source)) {
+    // 处理数组的情况
+    return source.map((item) => cloneDeep(item)) as T;
+  }
+
+  // 处理对象的情况
+  const clonedObj: Record<string, any> = {};
+
+  for (const key in source) {
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      clonedObj[key] = cloneDeep(source[key]);
+    }
+  }
+
+  return clonedObj as T;
+}
+
+export default cloneDeep;
