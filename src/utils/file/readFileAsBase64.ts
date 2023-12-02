f0139a38faae2bf1472b09f91e01562a1b8ad2bd
guowh <973886860@qq.com>
2023-12-02 15:34:04 +0800

    feat: 添加 readFileAsBase64 函数

---
+++ src/utils/file/readFileAsBase64.ts
@@ -0,0 +1,26 @@
+/**
+ * ### 文件转 base64
+ * @param file 要转换的文件
+ * @returns 返回 base64 编码
+ */
+function readFileAsBase64(file: File): Promise<string | null> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (typeof reader.result === "string") {
+        resolve(reader.result);
+      } else {
+        reject(new Error("文件转 base64 失败"));
+      }
+    };
+
+    reader.onerror = () => {
+      reject(new Error("读取文件失败"));
+    };
+
+    reader.readAsDataURL(file);
+  });
+}
+
+export default readFileAsBase64;
