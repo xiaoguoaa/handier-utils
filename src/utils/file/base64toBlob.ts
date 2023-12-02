7de9b886d145462ae72fb5d877a80d5870354888
guowh <973886860@qq.com>
2023-12-02 16:28:33 +0800

    feat: 优化 base64toBlob 函数

--- src/utils/file/base64toBlob.ts
+++ src/utils/file/base64toBlob.ts
@@ -1,13 +1,22 @@
+function getMimeTypeFromBase64(base64String: string): string {
+  const match = base64String.match(
+    /^data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+);base64,/
+  );
+
+  if (match && match[1]) {
+    return match[1];
+  }
+
+  return "application/octet-stream";
+}
+
 /**
  * ### base64转blob
  * @param base64String base64编码
- * @param contentType
+ * @param contentType 文件 mime 类型，有些特殊情况下取不到 mime 类型，这时候需要传入该参数
  * @returns 返回 Blob 对象
  */
-export function base64toBlob(
-  base64String: string,
-  contentType: string = "application/octet-stream"
-): Blob {
+export function base64toBlob(base64String: string, contentType?: string): Blob {
   const byteCharacters = atob(base64String);
   const byteArrays = [];
 
@@ -23,7 +32,9 @@ export function base64toBlob(
     byteArrays.push(byteArray);
   }
 
-  return new Blob(byteArrays, { type: contentType });
+  return new Blob(byteArrays, {
+    type: contentType || getMimeTypeFromBase64(base64String),
+  });
 }
 
 export default base64toBlob;
