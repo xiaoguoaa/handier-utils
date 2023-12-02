d4155d9ceb2e2df340f5114de063a58eb69cede5
guowh <973886860@qq.com>
2023-12-02 13:35:49 +0800

    feat: 添加 firstLetterUpper 函数

---
+++ src/utils/firstLetterUpper.ts
@@ -0,0 +1,10 @@
+/**
+ * ### 首字母大写
+ * @param str 要转换的字符串 `hello`
+ * @returns 转换后的字符串 `Hello`
+ */
+function firstLetterUpper(str: string) {
+  return str.at(0)?.toUpperCase() + str.slice(1);
+}
+
+export default firstLetterUpper;
