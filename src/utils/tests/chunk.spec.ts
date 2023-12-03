876b576bfd6fb4fd46becbadbfccb4e4a95488b5
guowh <973886860@qq.com>
2023-12-03 19:01:20 +0800

    test: chunk、clone 添加单元测试

---
+++ src/utils/tests/chunk.spec.ts
@@ -0,0 +1,43 @@
+import { expect, test, describe } from "vitest";
+import chunk from "../chunk";
+
+describe("数组拆分", () => {
+  test("正常情况", () => {
+    const inputArray = [1, 2, 3, 4, 5, 6, 7, 8];
+    const chunkedArray = chunk(inputArray, 3);
+
+    expect(chunkedArray).toEqual([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8],
+    ]);
+  });
+
+  test("空数组", () => {
+    const inputArray: any[] = [];
+    const chunkedArray = chunk(inputArray, 3);
+
+    expect(chunkedArray).toEqual([]);
+  });
+
+  test("size=0", () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const chunkedArray = chunk(inputArray, 0);
+
+    expect(chunkedArray).toEqual([]);
+  });
+
+  test("size > 数组长度", () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const chunkedArray = chunk(inputArray, 10);
+
+    expect(chunkedArray).toEqual([inputArray]);
+  });
+
+  test("size 为负数", () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const chunkedArray = chunk(inputArray, -2);
+
+    expect(chunkedArray).toEqual([]);
+  });
+});
---
+++ src/utils/tests/clone.spec.ts
@@ -0,0 +1,46 @@
+import { expect, test, describe } from "vitest";
+import clone from "../clone";
+
+describe("浅拷贝", () => {
+  test("源对象是 null", () => {
+    const result = clone(null);
+    expect(result).toBeNull();
+  });
+
+  test("源对象不是 object", () => {
+    const resultNumber = clone(42);
+    const resultString = clone("hello");
+    const resultBoolean = clone(true);
+
+    expect(resultNumber).toBe(42);
+    expect(resultString).toBe("hello");
+    expect(resultBoolean).toBe(true);
+  });
+
+  test("数组", () => {
+    const sourceArray = [1, 2, 3];
+    const result = clone(sourceArray);
+
+    expect(result).toEqual(sourceArray);
+    expect(result).not.toBe(sourceArray); // Check if test's a deep clone
+  });
+
+  test("对象", () => {
+    const sourceObject = { a: 1, b: { c: 2 } };
+    const result = clone(sourceObject);
+
+    expect(result).toEqual(sourceObject);
+    expect(result).not.toBe(sourceObject); // Check if test's a deep clone
+  });
+
+  test("嵌套对象", () => {
+    const sourceObject = { a: 1, b: { c: 2 } };
+
+    const result = clone(sourceObject);
+
+    expect(result.b).toEqual(sourceObject.b);
+    expect(result.b).toBe(sourceObject.b);
+  });
+
+  // Add more tests as needed
+});
