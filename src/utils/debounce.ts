c581e3a9a9d6c6817dec69f0f864b3d69bd68538
guowh <973886860@qq.com>
2023-12-05 11:20:47 +0800

    feat(bebounce、throttle): 修改 ts 类型声明

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,15 +1,13 @@
-import type { FunctionVoidWithThis, FunctionVoid } from "./types";
-
 /**
  * ### 防抖函数
  * @param func 要执行的函数
  * @param delay 延时时间(ms) `default=200`
  * @returns 返回防抖函数
  */
-function debounce<T extends FunctionVoid>(
+function debounce<T extends (...args: any[]) => void>(
   func: T,
   delay: number = 200
-): FunctionVoidWithThis<T> {
+): (this: ThisParameterType<T>, ...args: Parameters<T>) => void {
   let timeoutId: ReturnType<typeof setTimeout> | null;
 
   return function (this: ThisParameterType<T>, ...args: Parameters<T>): void {
--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,15 +1,13 @@
-import type { FunctionVoidWithThis, FunctionVoid } from "./types";
-
 /**
  * ### 节流函数
  * @param func 要执行的函数
  * @param delay 延时时间(ms) `default=200`
  * @returns 返回节流函数
  */
-function throttle<T extends FunctionVoid>(
+function throttle<T extends (...args: any[]) => void>(
   func: T,
   delay: number = 200
-): FunctionVoidWithThis<T> {
+): (this: ThisParameterType<T>, ...args: Parameters<T>) => void {
   let lastTime = 0;
 
   return function (this: ThisParameterType<T>, ...args: any[]) {
