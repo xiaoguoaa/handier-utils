eedfb322ed29ec1b9c354bae6cbf59d5fc39e3cc
guowh <973886860@qq.com>
2023-12-15 17:11:17 +0800

    chore: 删除冗余代码

--- src/utils/types/index.ts
+++
@@ -1,6 +0,0 @@
-export type FunctionVoidWithThis<T extends (...args: any[]) => any> = (
-  this: ThisParameterType<T>,
-  ...args: Parameters<T>
-) => void;
-
-export type FunctionVoid = (...args: any[]) => void;
